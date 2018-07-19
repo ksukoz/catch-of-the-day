2e408604adfd8b3d133d1a976178c9987226707b
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-07-19 13:49:40 +0300

    Create StorePicker component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+
+import './components/StorePicker';
+import StorePicker from './components/StorePicker';
+
+ReactDOM.render(<StorePicker />, document.getElementById('root'));
