427f9a1a0b195a236713b31be224fa17980d6676
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-07-19 15:23:06 +0300

    Add Header component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
+import './css/style.css';
+import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
 
-
-import './components/StorePicker';
 import StorePicker from './components/StorePicker';
 
-ReactDOM.render(<StorePicker />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
