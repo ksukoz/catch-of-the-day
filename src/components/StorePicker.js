7087212cd14e2ede77355e50d1a2eee4faf46a70
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-07-19 15:06:06 +0300

    Style StorePicker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,9 +1,10 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
+import './StorePicker.css';
 
-class StorePicker extends React.Component {
+class StorePicker extends Component {
   render() {
     return (
-        <form className="store-selector">
+        <form className="storePicker">
           <h2>Please Enter A Store</h2>
           <input type="text" placeholder="Store Name" required />
           <button type="submit">Visit Store &rarr;</button>
