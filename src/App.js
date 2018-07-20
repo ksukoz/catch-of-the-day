c543dfe417dd79b7a833147270d30fff01fdddc3
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-07-20 12:47:55 +0300

    Create Fish component and display fishes list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/Header';
 import Order from './components/Order';
 import Inventory from './components/Inventory.js';
+import sampleFishes from './sample-fishes';
+import Fish from './components/Fish';
 
 class App extends Component {
 
@@ -20,14 +22,22 @@ class App extends Component {
     console.log(this.state)
   };
 
+  loadSampleFishes = () => {
+    this.setState({fishes: sampleFishes})
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+            {/* <Fish /> */}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
   }
